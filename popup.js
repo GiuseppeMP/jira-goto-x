d342056459c7f3cf1299e2360894b424b505afad
Peppe <giuseppemath@gmail.com>
2023-09-11 10:46:50 -0300

    chore(validate): validate value hypotheses

--- popup.js
+++ popup.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('clickIt');
     checkPageButton.addEventListener('click', function() {
-  
-      chrome.tabs.getSelected(null, function(tab) {
-        alert("Hello..! It's my first chrome extension.");
-      });
+
+        chrome.tabs.getSelected(null, function(tab) {
+            const infoDisplay = document.getElementById('info');
+            var jira_template = "http://jiracielo.atlassian.net/browse/ARDS-" + infoDisplay.value;
+            chrome.tabs.create({ url: jira_template });
+        });
     }, false);
-  }, false);
+}, false);
