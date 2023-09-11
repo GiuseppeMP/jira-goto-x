f4e5b0af2a4b2c6e6a5a304ece9646caef4406da
Peppe <giuseppemath@gmail.com>
2023-09-11 10:57:52 -0300

    chore(validate): add enter submit

--- popup.js
+++ popup.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('clickIt');
+    var checkPageButton = document.getElementById('submit');
+
+    document.getElementById('info').addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            chrome.tabs.getSelected(null, function(tab) {
+                const infoDisplay = document.getElementById('info');
+                var jira_template = "http://jiracielo.atlassian.net/browse/ARDS-" + infoDisplay.value;
+                chrome.tabs.create({ url: jira_template });
+            });
+        }
+    });
     checkPageButton.addEventListener('click', function() {
 
         chrome.tabs.getSelected(null, function(tab) {
